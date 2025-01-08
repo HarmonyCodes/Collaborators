ec6ebe13b45b3ad65e046596e28aa60b867921ba
HarmonyCodes <nechamit887@gmail.com>
2025-01-08 21:57:50 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT || 4444
+app.get('/', (req, res)=>{
+    res.send("Hello world")
+})
+app.listen(PORT, ()=>{
+    console.log(`server running on port ${PORT}`);
+})
